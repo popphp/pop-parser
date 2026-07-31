9216f41fde5598a03ba0e4133b4a199d735a0d03
Nick Sagona <nick@nolainteractive.com>
2026-07-30 20:51:39 -0500

    Tests

---
+++ tests/AbstractParserTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Pop PHP Framework (https://www.popphp.org/)
+ *
+ * @link       https://github.com/popphp/popphp-framework
+ * @author     Nick Sagona, III <dev@noladev.com>
+ * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
+ * @license    https://www.popphp.org/license     New BSD License
+ */
+
+namespace Pop\Parser\Test;
+
+use Pop\Parser\ParserInterface;
+use Pop\Parser\Test\Fixtures\ConcreteParser;
+use PHPUnit\Framework\TestCase;
+
+class AbstractParserTest extends TestCase
+{
+
+    public function testImplementsParserInterface(): void
+    {
+        $parser = new ConcreteParser();
+        $this->assertInstanceOf(ParserInterface::class, $parser);
+    }
+
+    public function testConstructorWithoutDataLeavesDataNull(): void
+    {
+        $parser = new ConcreteParser();
+        $this->assertNull($parser->getData());
+    }
+
+    public function testConstructorWithDataSetsData(): void
+    {
+        $parser = new ConcreteParser('foo');
+        $this->assertEquals('foo', $parser->getData());
+    }
+
+    public function testSetDataIsFluentAndRetrievable(): void
+    {
+        $parser = new ConcreteParser();
+        $result = $parser->setData('bar');
+
+        $this->assertSame($parser, $result);
+        $this->assertEquals('bar', $parser->getData());
+    }
+
+    public function testGetResultDefaultsToNull(): void
+    {
+        $parser = new ConcreteParser();
+        $this->assertNull($parser->getResult());
+    }
+
+    public function testParseSetsResultAndReturnsStatic(): void
+    {
+        $parser  = new ConcreteParser('baz');
+        $result  = $parser->parse();
+
+        $this->assertSame($parser, $result);
+        $this->assertEquals('baz', $parser->getResult());
+    }
+
+    public function testHasErrorDefaultsToFalse(): void
+    {
+        $parser = new ConcreteParser();
+        $this->assertFalse($parser->hasError());
+    }
+
+    public function testGetErrorMessageDefaultsToNull(): void
+    {
+        $parser = new ConcreteParser();
+        $this->assertNull($parser->getErrorMessage());
+    }
+
+    public function testErrorStateIsExposedThroughGetters(): void
+    {
+        $parser = new ConcreteParser();
+        $parser->triggerError('Something went wrong.');
+
+        $this->assertTrue($parser->hasError());
+        $this->assertEquals('Something went wrong.', $parser->getErrorMessage());
+    }
+
+}
