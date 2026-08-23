933be81dbebb33f36c7a3b0e783e99bea52b1c44
Nick Sagona <nick@noladev.com>
2026-08-23 11:33:56 -0500

    Update email, license and copyrights

--- tests/Address/AddressValuesTest.php
+++ tests/Address/AddressValuesTest.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Pop PHP Framework (https://www.popphp.org/)
- *
- * @link       https://github.com/popphp/popphp-framework
- * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
- * @license    https://www.popphp.org/license     New BSD License
- */
 
 namespace Pop\Parser\Test\Address;
 
--- tests/Fixtures/ConcreteParser.php
+++ tests/Fixtures/ConcreteParser.php
@@ -1,21 +1,9 @@
 <?php
-/**
- * Pop PHP Framework (https://www.popphp.org/)
- *
- * @link       https://github.com/popphp/popphp-framework
- * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
- * @license    https://www.popphp.org/license     New BSD License
- */
 
 namespace Pop\Parser\Test\Fixtures;
 
 use Pop\Parser\AbstractParser;
 
-/**
- * Minimal concrete parser used to exercise AbstractParser, which cannot be
- * instantiated directly.
- */
 class ConcreteParser extends AbstractParser
 {
 
